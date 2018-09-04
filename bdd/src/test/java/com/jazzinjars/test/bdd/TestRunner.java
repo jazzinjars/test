7891db3faab3d5c896973bc3b7e0bb0862e244c8
Héctor Ardila López <hectorardila@gmail.com>
2018-09-04 16:56:44 +0200

    Adding Test Features

--- bdd/src/test/java/com/jazzinjars/test/bdd/TestRunner.java
+++ bdd/src/test/java/com/jazzinjars/test/bdd/TestRunner.java
@@ -1,8 +1,12 @@
 package com.jazzinjars.test.bdd;
 
 import cucumber.api.CucumberOptions;
+import cucumber.api.testng.CucumberFeatureWrapper;
 import cucumber.api.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 @CucumberOptions(
 		features = "src/test/resources/features",
@@ -18,8 +22,23 @@ public class TestRunner {
 	private TestNGCucumberRunner testNGCucumberRunner;
 
 	@BeforeClass(alwaysRun = true)
-	public void setUpClass() throws Exception {
+	public void setUpClass() {
 		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	}
+
+	@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+	public void feature(CucumberFeatureWrapper cucumberFeature) {
+		testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+	}
+
+	@DataProvider
+	public Object[][] features() {
+		return testNGCucumberRunner.provideFeatures();
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDownClass() throws Exception {
+		testNGCucumberRunner.finish();
 	}
 
 }
---
+++ bdd/src/test/java/com/jazzinjars/test/bdd/stepdefs/StepDefinitions.java
@@ -0,0 +1,4 @@
+package com.jazzinjars.test.bdd.stepdefs;
+
+public class StepDefinitions {
+}
